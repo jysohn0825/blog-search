f0c179144cb390289dcedb815fc6dca210580ce1
jysohn0825 <jysohn0825@naver.com>
2023-03-18 16:32:40 +0900

    chore : ktlint check

--- src/test/kotlin/com/jysohn0825/blog/BlogApplication.kt
+++ src/test/kotlin/com/jysohn0825/blog/BlogApplication.kt
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BlogApplication{
+class BlogApplication {
 
     @Test
-    fun contextLoads(){}
+    fun contextLoads() {
+    }
 }
