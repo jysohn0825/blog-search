c3cca20893c92e807072f2870a41b3ed91697a2c
jysohn0825 <jysohn0825@naver.com>
2023-03-21 19:38:34 +0900

    fix : 특정 블로그 조회 조건 확인
    - 검색 후 이벤트 발행 전 실제 검색어 조회 로직 수정

--- src/main/kotlin/com/jysohn0825/blog/application/KeywordSearchService.kt
+++ src/main/kotlin/com/jysohn0825/blog/application/KeywordSearchService.kt
@@ -3,7 +3,6 @@ package com.jysohn0825.blog.application
 import com.jysohn0825.blog.infra.channel.ChannelFactory
 import com.jysohn0825.blog.infra.channel.ChannelType
 import com.jysohn0825.support.domain.BasePageRequest
-import com.jysohn0825.support.utils.getStringYyMMddHH
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
@@ -14,12 +13,13 @@ class KeywordSearchService(
 ) {
 
     fun searchByKeyword(channel: ChannelType, keyword: String, pageRequest: BasePageRequest): KeywordSearchResponse {
-        countKeyword(keyword)
+        countKeyword(channel, keyword)
         val portalResponse = channelFactory.searchByKeyword(channel, keyword, pageRequest)
         return portalResponse.changeKeywordSearchResponse(pageRequest)
     }
 
-    private fun countKeyword(keyword: String) {
-        eventPublisher.publishEvent(PopularKeywordEvent(keyword))
+    private fun countKeyword(channel: ChannelType, keyword: String) {
+        val realKeyword = channelFactory.extractKeyword(channel, keyword)
+        eventPublisher.publishEvent(PopularKeywordEvent(realKeyword))
     }
 }
